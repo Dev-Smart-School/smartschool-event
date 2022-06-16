444bf5624bc485656dd89b417a1f1268d3c279f1
Lalu Abdurrahman <34390193+lalurahman@users.noreply.github.com>
2022-06-16 19:14:38 +0800

    cek login

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Guru;
 use App\Models\LoginRecord;
+use App\Models\Siswa;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,22 +13,22 @@ class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        $loginRecords = LoginRecord::all();
+        // dd($_GET['id']);
+        $user = User::find($_GET['id']);
+        $siswa = Siswa::find($_GET['id_siswa']);
+        $guru = Guru::find($_GET['id_guru']);
+        // $loginRecords = LoginRecord::where('user_id', $_GET['id'])->first();
+        $loginSiswa = LoginRecord::where('user_id', $_GET['id_siswa'])->first();
+        $loginGuru = LoginRecord::where('user_id', $guru->id_guru)->first();
         // // cek login record
         // // $ipAddress = $loginRecords->pluck('ip_address')->toArray();
         // // $userAgent = $request->header('User-Agent');
         // dd($loginRecords);
-        // // auth
-        // $user = User::all();
-        $user = User::where('id', $loginRecords->user_id)->first();
-        dd($user);
-        if ($user->id == $loginRecords->user_id && $user->type == 'siswa') {
-            dd('login siswa');
-            return view('welcome');
-        } else if($user->id == $loginRecords->user_id && $user->type == 'guru') {
-            dd('guru login');
-            return view('welcome');
+        
+        if ($loginSiswa) {
+            return view('pages.dashboard');
+        } else if($loginGuru) {
+            return view('pages.dashboard');
         }
-        return view('pages.dashboard');
     }
 }
